f7fbe55b214a7c513605d7d75c16b25a5807c230
qayumjon-coder <atajanovqayumjonbek@gmail.com>
2026-08-04 16:41:41 +0500

    Swiper bug fixes

--- swiper.js
+++ swiper.js
@@ -1,13 +1,31 @@
 const swiper = new Swiper('.first-swiper', {
-  slidesPerView: 4,
-  spaceBetween: 30,
-  grabCursor: true,
-  loop: true,
-  loopAddBlankSlides: false,
-  loopPreventsSliding: false,
+    slidesPerView: 4,
+    spaceBetween: 30,
+    grabCursor: true,
+    loop: true,
+    loopAddBlankSlides: false,
+    loopPreventsSliding: false,
 
-  navigation: {
-    nextEl: ".custom-btn-next",
-    prevEl: ".custom-btn-prev"
-  }
+    navigation: {
+        nextEl: ".custom-btn-next",
+        prevEl: ".custom-btn-prev"
+    }
 });
+
+const swiperSec = new Swiper('.second-swiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    grabCursor: true,
+    loop: true,
+
+    
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+
+    navigation: {
+        nextEl: ".second-btn-prev",
+        prevEl: ".second-btn-next"
+    }
+})
