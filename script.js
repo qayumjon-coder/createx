c3c018f64ec30b8d73d471aded5e8854499f599d
qayumjon-coder <atajanovqayumjonbek@gmail.com>
2026-08-06 17:50:11 +0500

    Footer started, Header's all features completed

--- script.js
+++ script.js
@@ -3,7 +3,19 @@ let actionBtns = document.querySelectorAll(".action-btn"),
     coursesGrid = document.querySelector(".courses_grid"),
     header = document.querySelector(".header"),
     slideBox = document.querySelector(".slide-box"),
-    blogGrid = document.querySelector(".blog-grid-container");
+    blogGrid = document.querySelector(".blog-grid-container"),
+    dropdownBtns = document.querySelectorAll(".dropdown-btn"),
+    drLink1 = document.querySelector(".dr-link"),
+    drLink2 = document.querySelector(".dr-link2"),
+    drLink3 = document.querySelector(".dr-link3"),
+    menuBtn = document.querySelector(".menu-btn"),
+    menu = document.querySelector(".menu"),
+    showMenu = document.querySelector(".show-menu"),
+    showMenu2 = document.querySelector(".show-menu2"),
+    showMenu3 = document.querySelector(".show-menu3"),
+    toggleMenuBtn = document.querySelector(".toggle-menu-btn"),
+    toggleMenuBtn2 = document.querySelector(".toggle-menu-btn2"),
+    toggleMenuBtn3 = document.querySelector(".toggle-menu-btn3");
 
 actionBtns.forEach(btn => {
     btn.addEventListener("click", () => {
@@ -128,6 +140,9 @@ let courseItem = courses.map(course => `
 
 coursesGrid.innerHTML = courseItem;
 
+
+// Header scroll event
+
 window.addEventListener("scroll", () => {
     if (window.scrollY > 88) {
         header.classList.add("scrolled");
@@ -200,6 +215,22 @@ let blogLatest = latestPosts.map(post => `
 
 blogGrid.innerHTML = blogLatest;
 
-/* 
-    
-*/
+
+// Menu and submenu opening and closing
+
+menuBtn.addEventListener("click", () => {
+    menu.classList.toggle("open");
+    menuBtn.innerHTML = '<i class="fa-solid fa-xmark text-[25px] text-secondary-text cursor-pointer"></i>';
+
+    if(!menu.classList.contains("open")){
+        menuBtn.innerHTML = '<i class="fa-solid fa-bars text-[20px] text-secondary-text cursor-pointer"></i>';
+    }
+});
+
+dropdownBtns.forEach((btn) => {
+    const submenu = btn.nextElementSibling;
+    btn.addEventListener("click", () => {
+        submenu.classList.toggle("open");
+        submenu.classList.toggle("hidden");
+    });
+});
