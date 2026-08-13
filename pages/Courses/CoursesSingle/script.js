5dcb119e469ac5347d3d75a69fd38b7f5b5628fb
boburshookh1 <boburshookh1@gmail.com>
2026-08-13 10:59:12 +0500

    rwb

--- pages/Courses/CoursesSingle/script.js
+++ pages/Courses/CoursesSingle/script.js
@@ -1,3 +1,5 @@
+
+
 const header = document.querySelector(".header");
 
 window.addEventListener("scroll", () => {
@@ -6,4 +8,29 @@ window.addEventListener("scroll", () => {
   } else {
     header.classList.remove("scrolled");
   }
+});
+
+
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("open");
+  menuBtn.innerHTML = '<i class="fa-solid fa-xmark text-[25px] text-secondary-text cursor-pointer"></i>';
+
+  if (!menu.classList.contains("open")) {
+    menuBtn.innerHTML = '<i class="fa-solid fa-bars text-[20px] text-secondary-text cursor-pointer"></i>';
+  }
+});
+
+dropdownBtns.forEach((btn) => {
+  const submenu = btn.nextElementSibling;
+  btn.addEventListener("click", () => {
+    submenu.classList.toggle("open");
+    submenu.classList.toggle("hidden");
+  });
+});
+
+backTop.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
 });
